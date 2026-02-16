c11970e676ff13a333345d28b8a591ecdbd7c24f
Robert Stefanic <rstefanic72@gmail.com>
2026-02-15 21:01:32 -0500

    main: Add `minimum_cursor_row_start`
    This is to ensure that the user's cursor isn't overlapping with the info
    report printed at the top of the terminal.
    
    Signed-off-by: Robert Stefanic <rstefanic72@gmail.com>

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <termios.h>
 #include <unistd.h>
 
 #include "base.h"
@@ -459,6 +460,51 @@ void eval_expressions(Expression *expressions) {
 	}
 }
 
+// Make sure the user's cursor isn't overwriting any data pinned to the top of
+// the terminal. If the cursor is currently set to be within this `min_row`,
+// then we'll move the cursor under it so the prompt will be fully visible.
+//
+// This is done by sending a Device Status Report (DSR) request to the terminal
+// to get the current cursor's position. If the cursor is under the `min_row`,
+// we'll adjust the cursor so the prompt is moved directly under it.
+void minimum_cursor_row_start(u8 min_row) {
+	// Get the termios, and turn off echo so the DSR isn't "typed" to stdout.
+	struct termios curr_termios, tmp_termios;
+	tcgetattr(STDIN_FILENO, &curr_termios);
+
+	// Copy the current termios into `tmp_termios` and turn echo off.
+	tmp_termios = curr_termios;
+	tmp_termios.c_lflag &= ~(ECHO | ICANON);
+	tcsetattr(STDIN_FILENO, TCSANOW, &tmp_termios);
+
+	// Issue DSR to ask for cursor position.
+	printf("\033[6n");
+	fflush(stdout);
+	
+	// The result comes in the format of "ESC[<row>;<col>R". We're just
+	// interested in the <row> argument here and will ignore <col>.
+	char dsr[32] = { 0 };	// buffer for reading the DSR
+	u32 i = 0;
+	char c;
+	while ((c = getchar()) != EOF && i < 31) {
+		dsr[i++] = c;
+		if (c == 'R')
+			break;
+	}
+
+	char *end; // end pointer for `strtol` call
+	u8 row = strtol(dsr+3, &end, 10);
+	assert(end != dsr);
+
+	// If the cursor is in the min row, then move it under the min_row.
+	if (row < min_row) {
+		printf("\033[%d;1H\033[K", min_row+1);
+	}
+
+	// Restore the old termios.
+	tcsetattr(STDIN_FILENO, TCSANOW, &curr_termios);
+}
+
 int main() {
 	arena_init(&arena, arena_backing_buffer, ARENA_BYTES_LEN);
 	arena_init(&symtable_arena, symtable_backing_buffer, SYMTABLE_ARENA_BYTES_LEN);
@@ -510,23 +556,27 @@ int main() {
 
 			char *green_foreground = "\033[38;5;46m";
 			printf("%s=== INFO REPORT ===\033[K\n", green_foreground);
+
+			u32 info_line_count = 1;
 			for (u32 i = 0; i < messages.len; i++) {
 				char c = messages.value[i];
 				if (c == '\n') {
 					printf("\033[K"); // clear to end of line
+					info_line_count++;
 				}
 				putchar(c);
 			}
+
 			char *reset_colors = "\033[0m";
 			printf("=== END INFO REPORT ===\033[K%s\n", reset_colors);
-			printf("\033[u");	// restore cursor position
+			info_line_count++;
 
-			// TODO: Fix issue on `clear` screen where user can
-			//	overwrite this info report section.
+			printf("\033[u");	// restore cursor position
+			minimum_cursor_row_start(info_line_count);
 		}
 
 		log_context_end();
 	}
 
 	return 0;
-}
+} 
